c7d9a23a0847fcc5ab034557bf7cb4fead0d4081
Priyanshu Udeniya <106472140+Priyanshuudeniya@users.noreply.github.com>
2024-04-10 05:13:05 +0530

    comment

---
+++ public/js/script.js
@@ -0,0 +1,24 @@
+// function openForm() {
+//     document.getElementById("myForm").style.display = "block";
+//   }
+  
+//   function closeForm() {
+//     document.getElementById("myForm").style.display = "none";
+//   }
+
+
+
+  var password = document.getElementById("password"), 
+  confirm_password = document.getElementById("confirm_password");
+
+function validatePassword(){
+  if(password.value != confirm_password.value) {
+    confirm_password.setCustomValidity("Passwords Don't Match");
+  } else {
+    confirm_password.setCustomValidity('');
+  }
+}
+
+password.onchange = validatePassword;
+confirm_password.onkeyup = validatePassword;
+
